c78872b0db8f722d7271fc2bb6b0a2143cbd7c6a
Benjamin Aaen Jørgensen <benjaminaaen@gmail.com>
2026-09-11 11:15:05 +0200

    Tilføj lige version.h til Windows-kompilering

---
+++ src/version.h
@@ -0,0 +1,11 @@
+#ifndef GLADE_VERSION_H
+#define GLADE_VERSION_H
+
+#define GLADE_MAJOR_VERSION 3
+#define GLADE_MINOR_VERSION 40
+#define GLADE_MICRO_VERSION 0
+
+/* Tilføjet til Windows-kompilering */
+#define VCS_VERSION "3.40.0"
+
+#endif /* GLADE_VERSION_H */
